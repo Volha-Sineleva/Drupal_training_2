f61d3c892c3491d02e00c3160d8110bb25d40296
Volha-Sineleva <Volha_Sineleva@epam.com>
2016-10-06 15:13:37 +0300

    Lesson 1

---
+++ sites/all/modules/custom/training/templates/training_template.tpl.php
@@ -0,0 +1,6 @@
+<ul><strong><?php echo $title ?></strong>
+  <li><a href="<?php echo $links[0]?>"><?php echo $items[0]?></a></li>
+  <li><a href="<?php echo $links[1]?>"><?php echo $items[1]?></a></li>
+  <li><a href="<?php echo $links[2]?>"><?php echo $items[2]?></a></li>
+  <li><a href="<?php echo $links[3]?>"><?php echo $items[3]?></a></li>
+</ul>
